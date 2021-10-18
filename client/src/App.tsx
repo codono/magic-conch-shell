a234868d2ef997a9c51d4283762bb950d5637bc9
codono <jay@networkdefines.com>
2021-10-18 18:11:41 +0900

    feat single file upload component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,40 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import { SingleImgInput } from "./component/SingleImgInput";
+import styled from "styled-components";
+
+const Nav = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 30px;
+  background-color: aliceblue;
+`;
+
+const Body = styled.div`
+  padding: 50px;
+`;
 
 function App() {
+  const history = createBrowserHistory();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav>
+        <button onClick={() => (window.location.href = "/")}>home</button>
+        <button onClick={() => (window.location.href = "/singleImgInput")}>
+          single img input
+        </button>
+      </Nav>
+      <Body>
+        <Router history={history}>
+          <Route path="/singleImgInput" component={SingleImgInput} />
+        </Router>
+      </Body>
     </div>
   );
 }
---
+++ client/src/component/SingleImgInput.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const UploadButton = styled.label`
+  padding: 5px;
+  background-color: aqua;
+`;
+
+const UploadInput = styled.input`
+  display: none;
+`;
+
+export const SingleImgInput = () => {
+  const [file, setFile] = useState<File | undefined>();
+  const uploadFileChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
+
+  return (
+    <>
+      <UploadButton htmlFor="imgInput">upload img</UploadButton>
+      <UploadInput
+        id="imgInput"
+        type="file"
+        onChange={(e) => uploadFileChange(e)}
+      />
+      {file ? <div>{file.name}</div> : null}
+    </>
+  );
+};
