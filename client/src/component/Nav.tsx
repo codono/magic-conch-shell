a729e8fb9bc3743278f436eaf4a0b24b78b4bef5
codono <jay@networkdefines.com>
2021-11-03 17:50:28 +0900

    fix single file upload

--- client/src/component/Nav.tsx
+++ client/src/component/Nav.tsx
@@ -6,26 +6,24 @@ const Navbody = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  padding: 20px;
+  padding: 20px 0px;
   margin-bottom: 20px;
   background-color: aliceblue;
 `;
 
 export const Nav = () => {
   return (
-    <>
-      <Navbody>
-        <button onClick={() => (window.location.href = "/")}>home</button>
-        <button onClick={() => (window.location.href = "/singleImgInput")}>
-          single img input
-        </button>
-        <button onClick={() => (window.location.href = "/dragAndDrop")}>
-          drag and drop
-        </button>
-        <button onClick={() => (window.location.href = "/carousel")}>
-          carousel
-        </button>
-      </Navbody>
-    </>
+    <Navbody>
+      <button onClick={() => (window.location.href = "/")}>home</button>
+      <button onClick={() => (window.location.href = "/singleImgInput")}>
+        single img input
+      </button>
+      <button onClick={() => (window.location.href = "/dragAndDrop")}>
+        drag and drop
+      </button>
+      <button onClick={() => (window.location.href = "/carousel")}>
+        carousel
+      </button>
+    </Navbody>
   );
 };
