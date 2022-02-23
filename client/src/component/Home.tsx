9ddd96adbbce3b25afc0309f4188ee2164d1f2d7
codono <codonopsis0130@gmail.com>
2022-02-23 18:37:28 +0900

    add lucky button

--- client/src/component/Home.tsx
+++ client/src/component/Home.tsx
@@ -3,9 +3,29 @@ import styled from "styled-components";
 import { Nav } from "./Nav";
 
 export const Home = () => {
+  const Lotto = () => {
+    const ILLHWAKCEONGEUM: number[] = [];
+
+    for (let i = 0; i < 6; i++) {
+      const num = Math.floor(Math.random() * (46 - 1)) + 1;
+      if (ILLHWAKCEONGEUM.indexOf(num) !== -1) {
+        i--;
+        continue;
+      } else {
+        ILLHWAKCEONGEUM.push(num);
+      }
+    }
+
+    alert(ILLHWAKCEONGEUM);
+    return ILLHWAKCEONGEUM;
+  };
+
   return (
     <>
       <Nav />
+      <div>
+        <img src="lucky.png" onClick={Lotto} />
+      </div>
     </>
   );
 };
